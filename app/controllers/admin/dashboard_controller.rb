31bf5af4151357fd24f9ed306b978a57b805e6a7
Alexander Evans <sander7989@gmail.com>
2023-12-01 23:11:48 -0700

    use rails config values rather than direct ENV vars

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
+  http_basic_authenticate_with name: Rails.configuration.admin[:username], password: Rails.configuration.admin[:password]
   
   def show
   end
