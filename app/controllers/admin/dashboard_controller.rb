6b3cac674e1fb055d5f361d7db686bdb1abd1145
Alexander Evans <sander7989@gmail.com>
2023-12-02 19:09:50 -0700

    replace placeholder dashboard with product and category info

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin[:username], password: Rails.configuration.admin[:password]
   
   def show
+    @product_count = Product.count
+    @categories_count = Category.count
   end
 end
